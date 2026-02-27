fb739da2cbebb08690676a1ef5d3b1ceb6687032
CrowDx2468 <147876251+CrowDx2468@users.noreply.github.com>
2026-02-27 21:37:41 +0300

    Add files via upload

---
+++ prompter.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <tuple>
+using namespace std;
+
+string prompter(string a, string* save) {
+    string result = "";
+    for (int i = 0;i < 30;i++) {
+        if (save[i].find(a) != string::npos) {
+            result = save[i];
+            break;
+        }
+       
+    }
+    if (result == "") {
+        for (int j = 0; j < 30; j++) {
+            if (save[j] == "") {
+                save[j] = a;
+                result = a;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    string save[30] = {};
+    cout << prompter("Why do you cry, Willy?", save) << endl;
+    cout << prompter("Why, Willy?", save) << endl;
+    cout << prompter("Why do", save) << endl;
+}
+
+// как правильно передать длинну массива в циклы функции?
